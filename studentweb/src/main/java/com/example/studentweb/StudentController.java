9e6535eaa296eb3e690b364feb441868c3939fb6
YallaJahnavi <yallajahnavi170@gmail.com>
2025-07-14 07:49:23 +0530

    Update StudentController.java

--- studentweb/src/main/java/com/example/studentweb/StudentController.java
+++ studentweb/src/main/java/com/example/studentweb/StudentController.java
@@ -11,17 +11,37 @@ public class StudentController {
 
     List<Student> studentList = new ArrayList<>();
 
+//    @GetMapping("/")
+//    public String home(Model model) {
+//        model.addAttribute("student", new Student());
+//        return "index"; // loads index.html
+//    }
+    
     @GetMapping("/")
-    public String home(Model model) {
+    public String landingPage() {
+        return "landing"; // loads landing.html
+    }
+    
+    @GetMapping("/form")
+    public String studentForm(Model model) {
         model.addAttribute("student", new Student());
         return "index"; // loads index.html
     }
 
+
     @PostMapping("/addStudent")
     public String addStudent(@ModelAttribute Student student, Model model) {
-        studentList.add(student);
+    	for (Student s : studentList) {
+            if (s.getId() == student.getId()) {
+                model.addAttribute("error", "User ID already exists!");
+                model.addAttribute("student", new Student()); // Reset form
+                return "index";
+            }
+        }
+    	
+    	studentList.add(student);
         model.addAttribute("message", "Student added successfully!");
-        model.addAttribute("student", new Student()); // reset form
+        model.addAttribute("student", new Student()); // Reset form
         return "index";
     }
 
